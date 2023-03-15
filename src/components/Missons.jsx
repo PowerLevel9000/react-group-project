4a6aa197e54a8dba9d5ec3e2aac841342e4c0ec5
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-15 08:22:02 +0530

    loader added

--- src/components/Missons.jsx
+++ src/components/Missons.jsx
@@ -4,16 +4,16 @@ import React from 'react';
 import { getMissions } from '../redux/missions/missonSlice';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { RotatingLines } from 'react-loader-spinner';
 
 const Missions = () => {
-  const {mission : {missions: mission}} = useSelector((store) => store);
+  const { mission: { missions: mission, isLoading: loading } } = useSelector((store) => store);
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getMissions());
-  },[]);
-  console.log(mission);
-  const missionComponents = mission.map((mission)=>
+  }, []);
+  console.log(mission, loading);
+  const missionComponents = mission.map((mission) =>
     <div>
       <h4>{mission.mission_name}</h4>
       <p>{mission.description}</p>
@@ -22,8 +22,19 @@ const Missions = () => {
   console.log(missionComponents)
   return (
     <div>
-      <h1>Hello from missions</h1>
-      {missionComponents}
+      {loading ? (
+        <div className="loader">
+          <RotatingLines strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75" />
+        </div>
+      ) : (
+        <>
+          <h1>Hello from missions</h1>
+          {missionComponents}
+        </>
+      )}
+
     </div>
   )
 };
