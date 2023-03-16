581f55c66645b90c5951df89cdcce527f24e4550
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-16 09:13:07 +0530

    testing initial

---
+++ src/__test__/HelloJoke.test.jsx
@@ -0,0 +1,6 @@
+describe("test the testing", ()=>{
+    test("hello joke",()=>{
+        const joke =   `i don't trust trust stairs they are always upto something`;
+        expect(joke).toBe(joke)
+    })
+})
